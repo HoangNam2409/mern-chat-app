77fe1e2266d66e0ef2bcda509842aacd769aa6c1
HoangNam2409 <namzinv123@gmail.com>
2024-02-22 00:02:43 +0700

    UI design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,25 @@
+import Conversation from "./Conversation"
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+        <Conversation />
+    </div>
+  )
+}
+
+export default Conversations
+
+
+// STATER CODE SNIPPET
+// import Conversation from "./Conversation"
+
+// const Conversations = () => {
+//   return (
+//     <div className="py-2 flex flex-col overflow-auto">
+//         <Conversation />
+//     </div>
+//   )
+// }
+
+// export default Conversations
